30a2e9e73f507c402cabdabf01a4cee75ad91b76
AryoBimoPrakoso <prakosoaryobimo@gmail.com>
2026-07-08 22:27:31 +0700

    buat generate data & matrix

---
+++ src/generate_data.py
@@ -0,0 +1,50 @@
+import random
+import pandas as pd
+
+JUMLAH_USER = 1024
+
+gender = ["L", "P"]
+
+daerah = [
+    "Jakarta","Bandung","Surabaya","Medan",
+    "Makassar","Bogor","Depok","Bekasi",
+    "Semarang","Yogyakarta","Tangerang",
+    "Palembang","Balikpapan","Pontianak",
+    "Denpasar","Padang","Batam","Manado"
+]
+
+kategori = [
+    "Film",
+    "Musik",
+    "Game",
+    "Olahraga",
+    "Kuliner",
+    "Travel",
+    "Teknologi",
+    "Fashion",
+    "Edukasi"
+]
+
+rows = []
+
+for i in range(JUMLAH_USER):
+
+    rows.append({
+
+        "UserID":f"U{i+1:04d}",
+        "Umur":random.randint(17,80),
+        "Gender":random.choice(gender),
+        "Daerah":random.choice(daerah),
+        "Kategori":random.choice(kategori)
+
+    })
+
+df = pd.DataFrame(rows)
+
+df.to_csv(
+    "users_1024.csv",
+    index=False,
+    encoding="utf-8-sig"
+)
+
+print(df.head())
---
+++ src/generate_matrix.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("users_1024.csv")
+
+n = len(df)
+
+matrix = np.zeros((n,n),dtype=np.int32)
+
+def similarity(a,b):
+
+    score = 0
+
+    # --------------------
+    # UMUR
+    # --------------------
+
+    selisih = abs(a["Umur"]-b["Umur"])
+
+    if selisih <=5:
+        score +=5
+
+    elif selisih<=10:
+        score +=3
+
+    elif selisih<=20:
+        score +=1
+
+
+    # --------------------
+    # GENDER
+    # --------------------
+
+    if a["Gender"]==b["Gender"]:
+        score +=1
+
+
+    # --------------------
+    # DAERAH
+    # --------------------
+
+    if a["Daerah"]==b["Daerah"]:
+        score +=7
+
+
+    # --------------------
+    # KATEGORI
+    # --------------------
+
+    if a["Kategori"]==b["Kategori"]:
+        score +=6
+
+    return score
+
+for i in range(n):
+
+    user_a = df.iloc[i]
+
+    for j in range(n):
+
+        user_b = df.iloc[j]
+
+        matrix[i,j] = similarity(user_a,user_b)
+
+pd.DataFrame(matrix).to_csv(
+    "similarity_matrix_1024.csv",
+    index=False,
+    header=False
+)
